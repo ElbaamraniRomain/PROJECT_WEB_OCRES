5383b2b7f0ee08b15413f3961924cdf320eaab7d
PierreDuong <pierre.duong@edu.ece.fr>
2020-11-29 16:41:25 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import dashboard from './widgets/dashboard'
+import dashboard from './widgets/Dashboard'
 import SignIn from './SignIn'
 import addDataApi from './addDataApi'
 import 'antd/dist/antd.css';
